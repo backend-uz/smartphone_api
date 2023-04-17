87f49736b4021fabaf7edfda7f3b3061986ae10a
JavohirJalilov <javohirjaliloff@gmail.com>
2023-04-17 18:59:15 +0500

    flask app

---
+++ app.py
@@ -0,0 +1,44 @@
+# simple flask app
+from flask import Flask, request
+from smartphonedb import DB
+db = DB('db.json')
+
+app = Flask(__name__)
+
+@app.route('/smartphone')
+def getBrads() -> dict:
+    """
+    Return list of brands
+    """
+    return {"brands":db.get_tables()}
+
+@app.route('/smartphone/<brand>')
+def getPhones(brand) -> dict:
+    """
+    Return list of phones by brand
+    """
+    return {"phones":db.get_phone_list(brand)}
+
+@app.route('/smartphone/<brand>/<int:idx>')
+def getPhone(brand,idx) -> dict:
+    """
+    Return phone data by brand
+    """
+    return db.getPhone(brand,idx)
+
+@app.route('/smartphone/<brand>',methods=['POST'])
+def addPhone(brand) -> dict:
+    """
+    Add new phone to the database
+    """
+    all_table = db.get_tables()
+    if brand in all_table:
+        data = request.get_json()
+        table = db.db.table(brand)
+        table.insert(data)
+        return {"status":"success"}
+    else:
+        return {"status":"failed","message":"Brand not found"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
