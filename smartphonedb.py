bb9bc02ff58c487d05c470f72a5dc9a0f4361159
JavohirJalilov <javohirjaliloff@gmail.com>
2023-04-17 18:59:24 +0500

    change

---
+++ smartphonedb.py
@@ -0,0 +1,29 @@
+from tinydb import TinyDB, Query
+
+class DB:
+    def __init__(self,path):
+        self.db = TinyDB(path)
+
+    def get_tables(self):
+        """
+        To get the list of all the tables in the database
+        """
+        return list(self.db.tables())
+        
+    def getPhone(self,brand,idx):
+        """
+        Return phone data by brand
+        args:
+            brand: str
+        return:
+            dict
+        """
+        table = self.db.table(brand)
+        return table.get(doc_id=idx)
+
+    def get_phone_list(self,brand):
+        """
+        Return phone list
+        """
+        table = self.db.table(brand)
+        return table.all()
